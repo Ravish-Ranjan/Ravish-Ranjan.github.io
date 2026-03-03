a93301bd57fd1ff7c248d950ce1a852ed20650f8
Ravish Ranjan <122588009+Ravish-Ranjan@users.noreply.github.com.>
2026-03-03 23:39:00 +0530

    DEPLOY : image optimization removed

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
 	/* config options here */
 	reactCompiler: true,
 	output: "export",
+	trailingSlash: true,
+	images: {
+		unoptimized: true,
+	},
 };
 
 export default nextConfig;
